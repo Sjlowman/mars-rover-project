e550eb0e57f1ff08437cb2690248bfe1d81c4783
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-13 20:17:06 +0100

    Create processPlateau function

--- src/plateau/calc.test.ts
+++ src/plateau/calc.test.ts
@@ -1,4 +1,4 @@
-import { add } from "../plateau/calc";
+import { allocatePlateauSize } from "../plateau/calc";
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
     expect(add(10, 5)).toBe(15);
