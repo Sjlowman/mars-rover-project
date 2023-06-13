5cacaead8038656775138d7d8f749efc19ba6d97
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-13 20:23:20 +0100

    Rename plateau file

--- src/plateau/plateau.test.ts
+++ src/plateau/plateau.test.ts
@@ -1,7 +1,7 @@
 import { allocatePlateauSize } from "../plateau/calc";
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+    expect(add(11, 5)).toBe(16);
   });
   it("should return 5 for add(2,3)", () => {
     expect(add(2, 3)).toBe(5);
