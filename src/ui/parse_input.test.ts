2393405bcd9b2eebf3333a3429ca69e72ca1eff1
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-14 15:53:29 +0100

    Implement parse Rover initial aspect

--- src/ui/parse_input.test.ts
+++ src/ui/parse_input.test.ts
@@ -1,6 +1,16 @@
 import { parseRoverInitialPositionInput } from "../ui/parse_input";
-describe("test add function", () => {
+import { parseRoverInitialAspectInput } from "../ui/parse_input";
+
+describe("test initial position function", () => {
   it("should return [1,2] if it receives '1 2 N'", () => {
     expect(parseRoverInitialPositionInput("1 2 N")).toStrictEqual([1, 2]);
+  });
+});
+describe("test initial aspect function", () => {
+  it("should return N if it receives '1 2 N'", () => {
+    expect(parseRoverInitialAspectInput("1 2 N")).toStrictEqual("N");
+  });
+  it("should return W if it receives '1 2 W'", () => {
+    expect(parseRoverInitialAspectInput("1 2 W")).toStrictEqual("W");
   });
 });
--- src/ui/parse_input.ts
+++ src/ui/parse_input.ts
@@ -1,3 +1,4 @@
+import { CompassDirection, COMPASS_DIRECTIONS } from "./parseInputTypes";
 export function parseRoverInitialPositionInput(
   input: string
 ): Grid | undefined {
@@ -15,6 +16,18 @@ export function parseRoverInitialPositionInput(
   const roverStartPosition: Grid = [0, 0];
   roverStartPosition.splice(0, 1, xPosition);
   roverStartPosition.splice(1, 1, yPosition);
-  // we know the input is valid so we can return a Hole
+  // we know the input is valid so we can return a Grid position
   return roverStartPosition;
+}
+
+export function parseRoverInitialAspectInput(
+  input: string
+): CompassDirection | undefined {
+  const roverInitialAspect = input.substring(4, 5);
+  const aspectMap = ["N", "E", "S", "W"];
+
+  // now we verify it's valid
+
+  // we know the input is valid so we can return a Hole
+  return COMPASS_DIRECTIONS[aspectMap.indexOf(roverInitialAspect)];
 }
