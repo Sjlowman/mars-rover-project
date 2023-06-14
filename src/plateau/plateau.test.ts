750ed6e508fcdc0a897169832240b4b1603f8bad
Sjlowman <sarah.j.lowman@gmail.com>
2023-06-14 12:43:23 +0100

    Created a types directory

--- src/plateau/plateau.test.ts
+++ src/plateau/plateau.test.ts
@@ -1,6 +1,12 @@
 import { allocatePlateauSize } from "../plateau/plateau";
+type PositiveNumber = number & { __type: "PositiveNumber" };
+function assertsPositiveNumber(value: number): asserts value is PositiveNumber {
+  if (value < 0) throw new Error("Value must be a positive number");
+}
 describe("test setting the plateau size", () => {
   it("Should  return msg to say it's been set if it receives two positive integers", () => {
-    expect(allocatePlateauSize(10, 7)).toBe("Plateau max boundary set");
+    expect(
+      allocatePlateauSize(10 as PositiveNumber, -7 as PositiveNumber)
+    ).toBe("Plateau max boundary set");
   });
 });
--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -1,3 +1,11 @@
-export function allocatePlateauSize(x: number, y: number): string {
+/* Need to complete this, not currently checking properly, problem with how I implement it  receiving two variables) */
+type PositiveNumber = number & { __type: "PositiveNumber" };
+function assertsPositiveNumber(value: number): asserts value is PositiveNumber {
+  if (value < 0) throw new Error("Value must be a positive number");
+}
+export function allocatePlateauSize(
+  x: PositiveNumber,
+  y: PositiveNumber
+): string {
   return "Plateau max boundary set";
 }
